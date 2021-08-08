5c430ca704426d7a93b995f2a0e9859df261fdfa
Baltraz <86964184+Killermaschine88@users.noreply.github.com>
2021-08-08 20:20:32 +0000

    Fixed restart

--- commands/Dev/restart.js
+++ commands/Dev/restart.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const config = require('../../config.json')
 
 module.exports = {
   name: "restart",
@@ -10,8 +11,8 @@ module.exports = {
   async execute(client, message, args) {
     if (message.author.id !== config.ownerID) return message.channel.send("Can't use this!")
 
-    const restart = new Discord.MessageEmbed().setColor('ORANGE').setDescription('Restarting . . .').setFooter('I will DM once im restarted')
-          message.channel.send({ embeds: [restart]})
+    const restart = new Discord.MessageEmbed().setColor('ORANGE').setDescription('Restarting . . .').setFooter('I will DM you once im restarted')
+          await message.channel.send({ embeds: [restart]})
 
     process.exit();
 
